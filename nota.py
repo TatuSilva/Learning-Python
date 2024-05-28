2e027ba9f0b434ed849752ce663af89c7724c257
tatu_silva <tainamoraes.br@gmail.com>
2024-05-28 20:12:40 -0300

    Testing the diference between if and elif

---
+++ nota.py
@@ -0,0 +1,11 @@
+nota=14
+
+if nota>10:
+    print("Nota inválida")
+elif nota==5:
+    print(f"Sua nota foi {nota}. Quase lá. Tente novamente")
+elif nota>5:
+    print(f"Você passou de ano e sua nota final é {nota}")
+else:
+    print(f"Você foi reprovado. Tente novamente.")
+
